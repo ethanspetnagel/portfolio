6f3016be8484bc63557536e5944f872385d8910e
ethanspetnagel <125224001+ethanspetnagel@users.noreply.github.com>
2025-07-06 22:23:57 -0700

    hm

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
         function showNextParagraph() {
             if (paragraphIndex < paragraphs.length) {
                 textElement.textContent = paragraphs[paragraphIndex];
-                textElement.style.color = '#ffffff'; // Set subtitle color to match project links
+                textElement.style.color = '#ec6632'; // Set subtitle color to match project links
                 
                 // Set custom delay: 2s for the second subtitle, 1.7s for others
                 const delay = (paragraphIndex === 1) ? 2600 : 1700;
